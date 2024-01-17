8729305906a7fdd99cc9773d81158959d6cc5b0a
Huangshuqi <1105374939@qq.com>
2024-01-17 20:04:54 +1100

    Upload the STL-Fan models code

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+from .convnext_utils import *
+from .swin_utils import *
+from .fan import fan_base_18_p16_224, fan_large_24_p16_224, fan_large_16_p4_hybrid, \
+    fan_tiny_8_p4_hybrid, fan_large_16_p4_hybrid_token, fan_tiny_8_p4_hybrid_token, \
+    fan_base_16_p4_hybrid_token, fan_base_16_p4_hybrid, fan_small_12_p4_hybrid_token, \
+    fan_small_12_p4_hybrid, fan_small_12_p16_224, fan_tiny_12_p16_224, \
+    fan_Xlarge_16_p4_hybrid, fan_small_12_p16_224_se_attn, fan_swin_small_patch4_window7_224, \
+    fan_swin_tiny_patch4_window7_224, fan_swin_base_patch4_window7_224, \
+    fan_swin_large_patch4_window7_224
